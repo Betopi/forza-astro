c9c9b4e03a3833e052684b48151667110a749c61
Ronaldo Montezuma <71199060+ronalmoctz@users.noreply.github.com>
2024-02-04 16:52:48 -0600

    galleria casi completa

--- src/components/CarrouselGallery.jsx
+++ src/components/CarrouselGallery.jsx
@@ -1,30 +1,46 @@
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/pagination";
-import "../../public/styles.css";
-import { Autoplay } from "swiper/modules";
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+import '../../public/styles.css';
+
+// import required modules
+import { Pagination } from 'swiper/modules';
 
 export function CarrouselGallery() {
   return (
-    <>
-      <Swiper slidesPerView={"auto"} spaceBetween={60} className="">
-        <SwiperSlide>
-          <img src="../../public/img/ch-2.webp" alt="Image 1" />
+    <> 
+    <div
+  class="flex bg-transparent items-center justify-center mb-20 mx-auto bg-black"
+>
+    <div class="w-20 mx-1 border-b-2 border-gray-300"></div>
+      <h3 class="fade fade-up text-4xl px-3 flex font-md text-main-red font-bold">
+        Galleria
+      </h3>
+    <div class="w-20 mx-1 border-b-2 border-gray-300"></div>
+    </div>
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide> <img src="../../public/img/ch-3.webp" alt="img" />
         </SwiperSlide>
-        <SwiperSlide>
-          <img src="../../public/img/ch-2.webp" alt="Image 2" />
+        <SwiperSlide> <img src="../../public/img/ch-3.webp" alt="img" />
         </SwiperSlide>
-        <SwiperSlide>
-          <img src="../../public/img/ch-2.webp" alt="Image 3" />
+        <SwiperSlide> <img src="../../public/img/ch-3.webp" alt="img" />
         </SwiperSlide>
-        <SwiperSlide>
-          <img src="../../public/img/ch-2.webp" alt="Image 4" />
+        <SwiperSlide> <img src="../../public/img/ch-3.webp" alt="img" />
         </SwiperSlide>
-        <SwiperSlide>
-          <img src="../../public/img/ch-2.webp" alt="Image 5" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="../../public/img/ch-2.webp" alt="Image 6" />
+        <SwiperSlide> <img src="../../public/img/ch-3.webp" alt="img" />
         </SwiperSlide>
       </Swiper>
     </>
